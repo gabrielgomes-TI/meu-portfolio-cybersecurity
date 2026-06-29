d62ba6e16d19575841184fbec0e46b524ee49716
gabrielgomes-TI <gabrielgomesdasilvagomes794@gmail.com>
2026-06-29 08:46:20 -0300

    Add setup script for data environment creation
    fecho iniciando a criação do ambiente de dados...
    criação das pastas estruturadas para o pipeline dos dados
    mkdir -p dados_ brutos dados_processados backups
    acho " pastas criadas com sucesso!"
    aplicando conceitos de segurança (triade CID-confidencialidade
    permissão 700: garante que apenas o dono do sistema assece os backups
    chmod 700 backups
    acho " segurança aplicada com sucesso na pasta de backup!"

---
+++ setup_dados.sh
@@ -0,0 +1,8 @@
+fecho iniciando a criação do ambiente de dados...
+criação das pastas estruturadas para o pipeline dos dados
+mkdir -p dados_ brutos dados_processados backups
+acho " pastas criadas com sucesso!"
+aplicando conceitos de segurança (triade CID-confidencialidade
+permissão 700: garante que apenas o dono do sistema assece os backups
+chmod 700 backups
+acho " segurança aplicada com sucesso na pasta de backup!"
